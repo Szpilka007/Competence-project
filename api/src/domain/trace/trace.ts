10ce83edfae96481d5e13fcc977be375589e0e63
TheDeMuZ <jakub.musial.001@gmail.com>
2020-11-30 03:08:58 +0100

    Added domain and entity classes for trace

---
+++ api/src/domain/trace/trace.ts
@@ -0,0 +1,25 @@
+export class Trace {
+  public constructor(
+    public id: string,
+    public personId: string,
+    public hotspotId: string,
+    public entryTime: Date,
+    public exitTime: Date
+  ) { }
+
+  public setPersonId(personId: string) {
+    this.personId = personId;
+  }
+
+  public setHotspotId(hotspotId: string) {
+    this.hotspotId = hotspotId;
+  }
+
+  public setEntryTime(entryTime: Date) {
+    this.entryTime = entryTime;
+  }
+
+  public setExitTime(exitTime: Date) {
+    this.exitTime = exitTime;
+  }
+}
