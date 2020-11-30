812d981ea2c4c342d10dc21f5e7cdb4b61c162a7
TheDeMuZ <jakub.musial.001@gmail.com>
2020-11-30 02:41:56 +0100

    Created migration for traces

---
+++ database/migrations/20201130024108_trace_model.js
@@ -0,0 +1,16 @@
+exports.up = async (knex) => {
+    await knex.raw('create extension if not exists "uuid-ossp"');
+    await knex.schema.createTable("traces", (table) => {
+        table.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
+        table.uuid("personId").references("id").inTable("persons").notNull();
+        table.uuid("hotspotId").references("id").inTable("hotspots").notNull();
+        table.timestamp("entryTime").notNull();
+        table.timestamp("exitTime").notNull();
+        table.timestamp("createdAt").defaultTo(knex.fn.now());
+        table.timestamp("updatedAt").defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = async (knex) => {
+    await knex.schema.dropTable("traces");
+};
